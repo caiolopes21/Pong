19b6eadd62da354537cc5b9ede4250bb97baaa94
caiolopes21 <caiolopesfaria21@gmail.com>
2019-09-17 13:45:02 -0300

    Initial commit

---
+++ src/pong/Enemy.java
@@ -0,0 +1,31 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Enemy {
+	
+	public double x,y;
+	public int width,height;
+	
+	public Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 5;
+	}
+
+	public void tick() {
+		x+= (Game.ball.x - x - 6) * 0.07;
+		if(x+width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect((int)x,(int)y, width, height);
+	}
+	
+}
